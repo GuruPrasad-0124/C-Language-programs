12420aec647b4c217b721f7b9a489c2cca0ebb97
GURU PRASAD PRASAD <96861135+GuruPrasad-0124@users.noreply.github.com>
2022-01-11 07:55:12 +0000

    nothiung

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int range,i;
-    printf("Enter range");
-    scanf("%d",&range);
-    for(i=1; i<=range; ++i)
-    {
-        if(i%2!=0)
-        {
-            printf("%d\n",i);
-        }
-    }
+  printf("hello world\n");
+  return 0;
 }
